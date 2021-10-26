cb5ee49c7c9988b484583df074a3d2cef21dd393
cgardner90 <calebgardner2015@gmail.com>
2021-10-26 15:23:23 -0600

    Spring REST ping test complete

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/UserController.java
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/UserController.java
@@ -1,12 +1,20 @@
 package com.skilldistillery.marketplace.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.marketplace.services.UserService;
 
 @RestController
+@RequestMapping("api")
 public class UserController {
 	@Autowired
 	private UserService userSvc;
+	
+	@GetMapping("ping")
+	public String ping() {
+		return "pong";
+	}
 }
--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/services/UserServiceImpl.java
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/services/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.skilldistillery.marketplace.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.marketplace.entities.User;
 import com.skilldistillery.marketplace.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepo;
