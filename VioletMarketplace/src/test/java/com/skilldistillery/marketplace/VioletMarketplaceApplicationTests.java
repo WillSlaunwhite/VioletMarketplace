81b607b4f5ad54234320d7009fbfc7e8e482c4ec
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-10-26 13:54:05 -0600

    Configure Boot project

---
+++ VioletMarketplace/src/test/java/com/skilldistillery/marketplace/VioletMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioletMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
