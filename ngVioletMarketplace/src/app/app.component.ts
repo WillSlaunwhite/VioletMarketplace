82e567741de32238e32b6a580d4adcd252c0cdca
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-10-30 09:57:48 -0600

    update navbar and login component to work as separate angular components

--- ngVioletMarketplace/src/app/app.component.ts
+++ ngVioletMarketplace/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loginUser = new User();
   title = 'ngVioletMarketplace';
 }
