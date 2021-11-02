9eff0dd480acd72db3610d84fedde478e9726a54
cgardner90 <calebgardner2015@gmail.com>
2021-11-02 12:15:02 -0600

    Functioning User Page

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/UserController.java
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/UserController.java
@@ -3,9 +3,11 @@ package com.skilldistillery.marketplace.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.marketplace.entities.User;
 import com.skilldistillery.marketplace.services.UserService;
 
 @RestController
@@ -15,8 +17,9 @@ public class UserController {
 	@Autowired
 	private UserService userSvc;
 	
-	@GetMapping("ping")
-	public String ping() {
-		return "pong";
+	@GetMapping("user/{username}")
+	public User returnUser(@PathVariable String username) {
+		User toReturn = userSvc.getUserByUsername(username);
+		return toReturn;
 	}
 }
