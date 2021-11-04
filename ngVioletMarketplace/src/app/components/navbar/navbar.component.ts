4b21370de866b739f2f6c8fbb8c0c22861259d2c
lohidenver <broadcastdave@gmail.com>
2021-11-03 20:37:43 -0600

    cards 22

--- ngVioletMarketplace/src/app/components/navbar/navbar.component.ts
+++ ngVioletMarketplace/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { BsModalRef, BsModalService, ModalOptions, ModalDirective } from 'ngx-bootstrap/modal';
 import { PictureuploadComponent } from '../pictureupload/pictureupload.component';
@@ -11,26 +11,30 @@ import { FormsModule } from '@angular/forms';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private auth: AuthService, private modalService: BsModalService) { }
-  bsModalRef?: BsModalRef;
+  constructor(private auth: AuthService,
+    private modalService: BsModalService,  modalRef: BsModalRef) { }
+  modalRef: BsModalRef= new BsModalRef();
   username: string | null = null;
   // check login
 
-  openModalWithComponent() {
-    const initialState: ModalOptions = {
-      initialState: {
-        list: [
-          'Open a modal with component',
-          'Pass your data',
-          'Do something else',
-          '...'
-        ],
-        title: 'Modal with component'
-      }
-    };
-    this.bsModalRef = this.modalService.show(PictureuploadComponent, initialState);
-    this.bsModalRef.content.closeBtnName = 'Close';
-  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+ }
+  // openModalWithComponent() {
+  //   const initialState: ModalOptions = {
+  //     initialState: {
+  //       list: [
+  //         'Open a modal with component',
+  //         'Pass your data',
+  //         'Do something else',
+  //         '...'
+  //       ],
+  //       title: 'Modal with component'
+  //     }
+  //   };
+  //   this.bsModalRef = this.modalService.show(PictureuploadComponent, initialState);
+  //   this.bsModalRef.content.closeBtnName = 'Close';
+  // }
 
  loggedIn(): boolean {
   return this.auth.isUserLoggedIn();
@@ -45,3 +49,6 @@ export class NavbarComponent implements OnInit {
   }
 
 }
+
+
+
