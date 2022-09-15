ea30ead008d5803ee5e911f89520cfdbde47446f
Will Slaunwhite <williamslaunwhite@gmail.com>
2022-09-14 18:16:57 -0700

    refreshing project

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/VioletMarketplaceApplication.java
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/VioletMarketplaceApplication.java
@@ -3,10 +3,12 @@ package com.skilldistillery.marketplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"src/main/java/com.skilldistillery.marketplace/controllers"})
 public class VioletMarketplaceApplication {
 
 	public static void main(String[] args) {
