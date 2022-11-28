7ab07b75ee976a856d1fc2c7621525b5d46baeb5
Will Slaunwhite <williamslaunwhite@gmail.com>
2022-11-28 00:39:49 -0800

    merged security branch into main

--- ngVioletMarketplace/src/app/components/home/home.component.ts
+++ ngVioletMarketplace/src/app/components/home/home.component.ts
@@ -23,19 +23,12 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
-    console.log(this.tokens.length);
-
     this.getTokens();
-
-    console.log(this.tokens.length);
-
   }
 
   getTokens(): void {
     this.tokenService.index().subscribe(
       tokenList => {
-        console.log(tokenList.length);
-
         this.tokens = tokenList;
       },
       failed => {
