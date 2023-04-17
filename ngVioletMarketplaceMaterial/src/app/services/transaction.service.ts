97cbf1ce87ac955fbe5b1f08365c9f22694c5662
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-04-17 13:23:49 -0700

    added methods from old transaction service

--- ngVioletMarketplaceMaterial/src/app/services/transaction.service.ts
+++ ngVioletMarketplaceMaterial/src/app/services/transaction.service.ts
@@ -1,9 +1,108 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Token } from 'typescript';
+import { Bid } from '../models/bid';
+import { AuthService } from './auth.service';
+import { TokenService } from './token.service';
+import Transaction from '../models/transaction';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransactionService {
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private tokenSvc: TokenService
+  ) {}
+  private baseUrl = 'http://localhost:8090/';
+  private url = this.baseUrl + 'api/';
 
-  constructor() { }
+  // what I'm thinking is, when you load the token page that has the list of bids
+  // onInit it will load the list of bids for each token and will also load a list of
+  // that token's transaction history
+
+  // you click the buy, we submit to purchase, you click on the make bid, it's tied to a
+  // click event that that calls the createBid() method on that page. That launches a form
+  // that will allow you to fill in the required information to create a new bid. The bid should
+  // be able to infer the buyer and seller information on the page.
+
+  // this way, when a user navigates to a token's page, we can have it set so when they click on
+  // the bids for it, they can see the pending bids yet to be accepted or denied, and on the other side,
+  // if you've submitted a bid for a bet when you navigate to the page it'll show the status of your bet.
+  // you'll also be able to see a concise list of these on the user's page.
+
+  // logic to create and update bids and transactions
+
+  create(bid: Bid): Observable<Bid> {
+    bid.accepted = false;
+    return this.http.post<Bid>(this.url, bid, this.auth.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('transactionService.create(): Error creating bid');
+      })
+    );
+  }
+
+  // logic to set users and tokens and transactions
+
+  // search for transfers by buyer seller and then all user transactions
+
+  getAllTransfers(): Observable<Transaction[]> {
+    return this.http
+      .get<Transaction[]>(this.url + 'transfers/1', this.auth.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            'transactionService.getAllTransfers(): Error retrieving Token Transaction list'
+          );
+        })
+      );
+  }
+
+  getBuyerTransfers(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'transactionService.getBuyerTransfers(): Error retrieving Token Transaction list'
+        );
+      })
+    );
+  }
+
+  getSellerTransfers(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'transactionService.getSellerTransfers(): Error retrieving Token Transaction list'
+        );
+      })
+    );
+  }
+
+  getAllUserTransfers(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'transactionService.getAllUserTransfers(): Error retrieving Token Transaction list'
+        );
+      })
+    );
+  }
+
+  getAllBids(): Observable<Bid[]> {
+    return this.http.get<Bid[]>(this.url + 'bids/1').pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'transactionService.getAllBids(): Error retrieving Bid list'
+        );
+      })
+    );
+  }
 }
