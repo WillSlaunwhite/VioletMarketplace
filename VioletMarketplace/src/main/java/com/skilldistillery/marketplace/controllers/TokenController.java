113e338d08cb862845054c8348d93e4abe1b2256
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-04-17 10:36:24 -0700

    reformat TokenController methods to properly handle requests

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/TokenController.java
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/TokenController.java
@@ -1,163 +1,150 @@
 package com.skilldistillery.marketplace.controllers;
 
-import java.security.Principal;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.skilldistillery.marketplace.entities.Token;
+import com.skilldistillery.marketplace.services.TokenService;
+import com.skilldistillery.marketplace.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.skilldistillery.marketplace.entities.Token;
-import com.skilldistillery.marketplace.entities.User;
-import com.skilldistillery.marketplace.services.TokenService;
-import com.skilldistillery.marketplace.services.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin({"*", "http://localhost:4301" })
+@CrossOrigin({"*", "http://localhost:4301"})
 public class TokenController {
-	@Autowired
-	private TokenService tokenSvc;
-	@Autowired
-	private UserService userSvc;
-	
-	
-	/////////////// UNAUTH METHODS ///////////////////
+    @Autowired
+    private TokenService tokenSvc;
+    @Autowired
+    private UserService userSvc;
 
 
-
-//	return all tokens
-@GetMapping("home/tokens")
-public ResponseEntity<Set<Token>> indexHome(HttpServletRequest req, HttpServletResponse resp) {
-	try {
-		Set<Token> tokenList = tokenSvc.index();
-		if (tokenList.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok(tokenList);
-	} catch (Exception e) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	}
-}
+    /////////////// UNAUTH METHODS ///////////////////
 
 
-	//	find non-principal user's tokens index method
-	@GetMapping("tokens/user/{username}")
-	public Set<Token> indexNonPrincipal(HttpServletRequest req,
-			HttpServletResponse resp,
-			@PathVariable String username) {
-		return tokenSvc.indexByUsername(username);
-	}
-
-//	Get a specific token by id
-	@GetMapping("tokens/id/{tid}")
-	public Token show(HttpServletRequest req,
-			HttpServletResponse resp,
-
-			@PathVariable int tid) {
-		return tokenSvc.showById(tid);
-	}
-
-
-
-	/////////////// GET METHODS ///////////////////
+    //	return all tokens
+    @GetMapping("home/tokens")
+    public ResponseEntity<Set<Token>> indexHome() {
+        ResponseEntity<Set<Token>> result;
+        try {
+            Set<Token> tokenList = tokenSvc.index();
+            if (tokenList.isEmpty()) {
+                result = ResponseEntity.notFound().build();
+            } else {
+                result = ResponseEntity.ok(tokenList);
+            }
+        } catch (Exception e) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return result;
+    }
 
 
+    //	find non-principal user's tokens index method
+    @GetMapping("tokens/user/{username}")
+    public ResponseEntity<Set<Token>> indexNonPrincipal(@PathVariable String username) {
+        try {
+            Set<Token> tokenList = tokenSvc.indexByUsername(username);
+            if (tokenList.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(tokenList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-//	Get principal's list of tokens
-	@GetMapping("tokens/myTokens")
-	public Set<Token> indexByUsername(HttpServletRequest req,
-			HttpServletResponse resp,
-			Principal principal) {
-		return tokenSvc.indexByUsername(principal.getName());
-	}
+    //	Get a specific token by id
+    @GetMapping("tokens/id/{tid}")
+    public ResponseEntity<Token> show(@PathVariable int tid) {
+        try {
+            Token token = tokenSvc.showById(tid);
+            if (token == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
-	
-	
-	/////////////// POST METHODS ///////////////////
-	
-	
-	
-	@PostMapping("tokens")
-	public Token create(HttpServletRequest req,
-			HttpServletResponse resp, 
-			Principal principal,
-			@RequestBody Token token) {
-		tokenSvc.create(principal.getName(), token);
-		if (token == null) {
-			resp.setStatus(404);
-		}
-		return token;
-	}
-	
-	
-	
-	/////////////// PUT METHODS ///////////////////
-	
-	
-	
-	// method has extra params it probably doesn't need 
-	@PutMapping("tokens/{tid}")
-	public Token purchaseToken(HttpServletRequest req,
-		HttpServletResponse resp,
-		Principal principal,
-		@PathVariable int tid) {
-		Token token = tokenSvc.showByUsernameId(principal.getName(), tid);
 
-		if (token == null) {
-			resp.setStatus(404);
-		}
-		
-		token = tokenSvc.update(principal.getName(), "dave", tid, token);
-		return token;
-	}
-	
-//	@PutMapping("tokens/{tid}")
-//	public Token updateMyToken(HttpServletRequest req,
-//			HttpServletResponse resp,
-//			Principal principal,
-//			@PathVariable int tid) {
-//		
-//		Token token = new Token();
-//		token = tokenSvc.show(principal.getName(), tid);
-//		
-//		if (token == null) {
-//			resp.setStatus(404);
-//		}
-//		
-//		token = tokenSvc.update(principal.getName(), tid, token);
-//		return token;
-//	}
-	
-	
-	
-	/////////////// DELETE METHODS ///////////////////
-	
-	
-	@DeleteMapping("tokens/{tid}")
-	public void destroy(HttpServletRequest req,
-		HttpServletResponse resp,
-		Principal principal,
-		@PathVariable int tid) {
+    /////////////// GET METHODS ///////////////////
 
-		if (tokenSvc.destroy(principal.getName(), tid)) {
-			resp.setStatus(204);
-		} else {
-			resp.setStatus(404);
-		}
-	}
-	
-	
+
+    //	Get principal's list of tokens
+    @GetMapping("tokens/myTokens")
+    public ResponseEntity<Set<Token>> indexByUsername(Principal principal) {
+        try {
+            Set<Token> tokenList = tokenSvc.indexByUsername(principal.getName());
+            if (tokenList.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(tokenList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+    /////////////// POST METHODS ///////////////////
+
+
+    @PostMapping("tokens")
+    public ResponseEntity<Token> create(Principal principal,
+                                        @RequestBody Token token) {
+        try {
+            if (token == null) {
+                return ResponseEntity.badRequest().build();
+            }
+            tokenSvc.create(principal.getName(), token);
+            return ResponseEntity.status(HttpStatus.CREATED).body(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+    /////////////// PUT METHODS ///////////////////
+
+
+    // method has extra params it probably doesn't need
+    @PutMapping("tokens/{tid}")
+    public ResponseEntity<Token> purchaseToken(Principal principal,
+                                               @PathVariable int tid) {
+        try {
+            Token token = tokenSvc.showByUsernameId(principal.getName(), tid);
+            if (token == null) {
+                return ResponseEntity.notFound().build();
+            }
+            token = tokenSvc.update(principal.getName(), "dave", tid, token);
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+    /////////////// DELETE METHODS ///////////////////
+
+
+    @DeleteMapping("tokens/{tid}")
+    public ResponseEntity<Void> destroy(HttpServletResponse resp, Principal principal, @PathVariable int tid) {
+        try {
+            boolean isDeleted = tokenSvc.destroy(principal.getName(), tid);
+            if (isDeleted) {
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
 }
