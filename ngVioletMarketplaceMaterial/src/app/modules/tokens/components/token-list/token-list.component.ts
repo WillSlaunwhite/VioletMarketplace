b120f976b852bdb6e4e4e98a56a91810bd0a5224
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-05-20 21:38:01 -0700

    improved token and user observables, styling in token-list

--- ngVioletMarketplaceMaterial/src/app/modules/tokens/components/token-list/token-list.component.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/tokens/components/token-list/token-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import Token from 'src/app/models/token';
-import { Observable, filter, of, tap } from 'rxjs';
+import { Observable, filter, of, take, tap, withLatestFrom } from 'rxjs';
 import { getAllTokens, getUserTokens } from '../../state/tokens.selectors';
 import { loadTokens, loadUserTokens } from '../../state/tokens.actions';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import User from 'src/app/models/user';
 
 @Component({
   selector: 'app-token-list',
@@ -14,32 +15,29 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 export class TokenListComponent implements OnInit {
   tokens$: Observable<Token[] | null> = of(null);
 
-  constructor(private store: Store, private auth: AuthService) {
-    // this.tokens$ = this.store.select(getAllTokens).pipe(
-    //   filter(tokens => tokens !== null)
-    // );
-    this.auth.currentUser.subscribe(
-      user => {
-        if (user) {
-          this.store.dispatch(loadUserTokens({ username: 'will' }));
-          this.tokens$ = this.store.select(getUserTokens)
-        } else {
-          this.tokens$ = this.store.select(getAllTokens);
-        }
-      }
-    );
-  }
+  constructor(private store: Store, private auth: AuthService) { }
 
   ngOnInit(): void {
-    this.store.dispatch(loadTokens());
-    this.auth.currentUser.subscribe(
-      user => {
-        if (user) {
-          this.store.dispatch(loadUserTokens({ username: 'will' }));
-          this.tokens$ = this.store.select(getUserTokens)
+    this.tokens$ = this.store.select(getUserTokens);
+
+    this.tokens$.pipe(
+      take(1),
+      tap((tokens: Token[] | null) => {
+        if (!tokens) {
+          this.store.dispatch(loadTokens());
         }
-      }
-    );
+      })
+    ).subscribe();
+
+    this.auth.currentUser.pipe(
+      filter(user => user !== null),  // * ensure user is logged in
+      withLatestFrom(this.tokens$),   // * Combine with tokens$
+      tap(([user, tokens]: [User | null, Token[] | null]) => {       // * if tokens$ is empty 
+        if (!tokens && user) {        // * we load user's tokens
+          this.store.dispatch(loadUserTokens({ username: user.username }));
+        }
+      })
+    ).subscribe();
   }
 
 }
