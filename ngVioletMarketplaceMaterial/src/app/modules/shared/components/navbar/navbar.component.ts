b92d0a56efef667dca793a903ad7edd3eee9fc3d
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-05-25 22:54:48 -0700

    problems hiding/showing correct buttons

--- ngVioletMarketplaceMaterial/src/app/modules/shared/components/navbar/navbar.component.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import User from 'src/app/models/user';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDivider, MatDividerModule } from '@angular/material/divider';
@@ -19,24 +19,28 @@ export class NavbarComponent implements OnInit {
   username: string | null = null;
   user: User | null = new User();
   searchTerm: string | null = null;
-  isLoggedIn$: Observable<boolean>;
+  isLoggedIn$: Observable<boolean> = of(false);
   user$: Observable<User | null>;
   isSearchFieldFocused: boolean = false;
 
   constructor(private store: Store, private dialog: MatDialog, private renderer: Renderer2, private el: ElementRef) {
     this.user$ = this.store.select(selectCurrentUser);
     this.isLoggedIn$ = this.store.select(isLoggedIn);
-  }
-
-  logout(): void {
-    this.store.dispatch(logout());
-    window.location.reload();
+    console.log(this.store.select(isLoggedIn));
   }
 
 
   ngOnInit(): void {
+    this.isLoggedIn$ = this.store.select(isLoggedIn);
   }
 
+
+
+  logout(): void {
+    this.store.dispatch(logout());
+    this.isLoggedIn$ = this.store.select(isLoggedIn);
+    window.location.reload();
+  }
 
   openRegisterDialog(): void {
     this.dialog.open(RegisterComponent);
@@ -46,6 +50,17 @@ export class NavbarComponent implements OnInit {
     this.dialog.open(LoginComponent);
   }
 
+  openRegisterDialogAction(): Function {
+    return () => this.openRegisterDialog();
+  }
+
+  logoutAction(): Function {
+    return () => this.logout();
+  }
+
+  openLoginDialogAction(): Function {
+    return () => this.openLoginDialog();
+  }
 
   toggleSearchFocus() {
     this.isSearchFieldFocused = !this.isSearchFieldFocused;
