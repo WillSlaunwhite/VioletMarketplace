3481f57a33dad000a9c74fee8107e043b1dfbc87
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-06-06 23:11:03 -0700

    added animation to register, moved dialog-bg

--- ngVioletMarketplaceMaterial/src/app/modules/auth/components/alt-register/alt-register.component.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/auth/components/alt-register/alt-register.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInTop } from 'src/app/animations/animations';
 
 @Component({
   selector: 'app-alt-register',
   templateUrl: './alt-register.component.html',
-  styleUrls: ['./alt-register.component.scss']
+  styleUrls: ['./alt-register.component.scss'],
+  animations: [slideInTop]
 })
 export class AltRegisterComponent implements OnInit {
 
