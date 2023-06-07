bc6200e475f0159f645598ec01f3a82bd4c3ce13
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-06-07 01:04:20 -0700

    alt register super rough draft (fields added)

--- ngVioletMarketplaceMaterial/src/app/modules/auth/components/alt-register/alt-register.component.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/auth/components/alt-register/alt-register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { slideInTop } from 'src/app/animations/animations';
+import User from 'src/app/models/user';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from 'src/app/modules/shared/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alt-register',
@@ -8,10 +12,36 @@ import { slideInTop } from 'src/app/animations/animations';
   animations: [slideInTop]
 })
 export class AltRegisterComponent implements OnInit {
+  registerUser: User = new User();
+  confirmPassword: string | null = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  register(): void {
+    this.userService.register(this.registerUser).subscribe(
+      () => {
+        this.loginNewUser(this.registerUser);
+      },
+      (error) => {
+        console.error(
+          'RegisterComponent.register(): registration failed\n' + error
+        );
+      }
+    );
+  }
+
+  loginNewUser(user: User): void {
+    this.authService.login(user.username, user.password).subscribe(
+      (success) => {
+        this.router.navigateByUrl('/home');
+      },
+      (error) => {
+        console.error('RegisterComponent.loginNewUser: login failed\n' + error);
+      }
+    );
   }
 
 }
