5e77bccc424d19ffa36e7549ac8fb8af9ede3a10
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-06-19 14:33:38 -0700

    fixed register/user module issues removed email-username and password-container

--- ngVioletMarketplaceMaterial/src/app/modules/register/register.module.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/register/register.module.ts
@@ -3,25 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { AltRegisterComponent } from './components/register/alt-register/alt-register.component';
-import { EmailUsernameContainerComponent } from './components/register/email-username-container/email-username-container.component';
 import { OptionalFieldsContainerComponent } from './components/register/optional-fields-container/optional-fields-container.component';
 import { OptionalFormComponent } from './components/register/optional-form/optional-form.component';
-import { PasswordContainerComponent } from './components/register/password-container/password-container.component';
 import { RegisterComponent } from './components/register/register/register.component';
 import { RegisterTabContainerComponent } from './components/register/register-tab-container/register-tab-container.component';
 import { RequiredFormComponent } from './components/register/required-form/required-form.component';
 import { SummaryTabComponent } from './components/register/summary-tab/summary-tab.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AltRegisterComponent,
-    EmailUsernameContainerComponent,
     OptionalFieldsContainerComponent,
     OptionalFormComponent,
-    PasswordContainerComponent,
     RegisterComponent,
     RegisterTabContainerComponent,
     RequiredFormComponent,
@@ -31,10 +28,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     RegisterRoutingModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   exports: [
-
+    RegisterComponent
   ]
 })
 export class RegisterModule { }
