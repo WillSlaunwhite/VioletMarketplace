05da38d4451d73ed2c86d55da4807d611f718b92
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-07-01 18:38:53 -0700

    generated login components, updated necessary import/exports, deleted dead components

--- ngVioletMarketplaceMaterial/src/app/modules/login/login.module.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/login/login.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { SocialMediaContainerComponent } from './components/social-media-container/social-media-container.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SocialMediaContainerComponent,
+    LoginFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    SharedModule
+  ],
+  exports: [
+    LoginComponent
   ]
 })
 export class LoginModule { }
