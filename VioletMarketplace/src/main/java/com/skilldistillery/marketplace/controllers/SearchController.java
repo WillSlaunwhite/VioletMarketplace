76b089c7a4d0d5373df6c00dd1b60306c5b617c7
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-07-13 17:43:36 -0700

    tested search endpoint successfully

---
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/controllers/SearchController.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.marketplace.controllers;
+
+import com.skilldistillery.marketplace.interfaces.Searchable;
+import com.skilldistillery.marketplace.services.SearchServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4301"})
+public class SearchController {
+    @Autowired
+    private SearchServiceImpl searchService;
+
+    @GetMapping("/search/{query}")
+    public List<Searchable> search(@PathVariable String query) {
+        return searchService.search(query);
+    }
+}
