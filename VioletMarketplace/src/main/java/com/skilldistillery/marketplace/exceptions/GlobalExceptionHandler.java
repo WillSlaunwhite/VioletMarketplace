89e86a569ebf79dcb66ee569e22ce57842cd6e1e
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-07-18 13:40:31 -0700

    creaetd custom exceptions and global exception handler

---
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.skilldistillery.marketplace.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ResponseEntity<Void> handleTokenNotFoundException(TokenNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Void> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleTokenNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+}
