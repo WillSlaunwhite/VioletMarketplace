9c481eb1b8e66808ad481a1bbba060e45f6c7878
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-07-30 18:39:17 -0700

    all basic user-balance endpoints working

---
+++ ngVioletMarketplaceMaterial/src/app/modules/user/modules/user-balance/user-balance.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BalanceDisplayComponent } from './components/balance-display/balance-display.component';
+
+
+
+@NgModule({
+  declarations: [
+    BalanceDisplayComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserBalanceModule { }
