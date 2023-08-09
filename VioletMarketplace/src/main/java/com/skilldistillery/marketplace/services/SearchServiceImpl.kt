3d83341f5766ac3e47f7664d0630621fc1a1f192
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-09 16:38:57 -0700

    refactored search layer to work with trie

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/services/SearchServiceImpl.kt
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/services/SearchServiceImpl.kt
@@ -1,14 +1,11 @@
 package com.skilldistillery.marketplace.services
 
-import com.skilldistillery.marketplace.dto.SearchResults
+import com.skilldistillery.marketplace.dtos.SearchResults
 import com.skilldistillery.marketplace.entities.Token
 import com.skilldistillery.marketplace.entities.User
-import com.skilldistillery.marketplace.enums.AccountStatus
-import com.skilldistillery.marketplace.enums.Status
 import com.skilldistillery.marketplace.repositories.TokenRepository
 import com.skilldistillery.marketplace.repositories.TrieRepository
 import com.skilldistillery.marketplace.repositories.UserRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,10 +13,9 @@ class SearchServiceImpl(private val userRepo: UserRepository, private val tokenR
     // TODO removed all check from function, need to adjust accordingly
     override fun search(query: String): SearchResults {
         val trieResults = trieRepo.trie.search(query)
-        println(trieResults)
-
-        val tokenResults = trieResults.filter { it.getType() == "token" }.map { it as Token }.toSet()
-        val userResults = trieResults.filter { it.getType() == "user" }.map { it as User }.toSet()
+        trieResults.forEach { println("trie results: ${it.description}") }
+        val tokenResults = trieResults.filter { it.type == "Token" }.map { it as Token }.toSet()
+        val userResults = trieResults.filter { it.type == "User" }.map { it as User }.toSet()
         return SearchResults(userResults, tokenResults)
     }
 }
