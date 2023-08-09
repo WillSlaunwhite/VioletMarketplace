f9a411db1d75e20a724ca3700da4fa97d65d4a35
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-09 16:17:53 -0700

    token refactor

--- MarketplaceJPA/src/main/java/com/skilldistillery/marketplace/entities/Token.kt
+++ MarketplaceJPA/src/main/java/com/skilldistillery/marketplace/entities/Token.kt
@@ -1,6 +1,5 @@
 package com.skilldistillery.marketplace.entities
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.skilldistillery.marketplace.converters.RarityConverter
 import com.skilldistillery.marketplace.converters.StatusConverter
 import com.skilldistillery.marketplace.enums.Rarity
@@ -9,93 +8,34 @@ import com.skilldistillery.marketplace.interfaces.Searchable
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDate
-import java.util.*
 import javax.persistence.*
 
 @Entity
-class Token : Searchable {
-    @JvmField
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id = 0
-    @JvmField
-    var name: String? = null
-    private var description: String? = null
-    @JvmField
-    var price = 0
-
-    @JvmField
+data class Token(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0,
+    var name: String,
+    var description: String,
+    var price: Int = 0,
     @Convert(converter = StatusConverter::class)
-    var status: Status? = null
-
-    @JvmField
+    var status: Status,
     @Convert(converter = RarityConverter::class)
-    var rarity: Rarity? = null
-
-    @CreationTimestamp
-    @Column(name = "release_date")
-    var releaseDate: LocalDate? = null
-
-    @JvmField
-    @UpdateTimestamp
-    @Column(name = "updated_on")
-    var updatedOn: LocalDate? = null
-
-    @JvmField
+    var rarity: Rarity,
+    @CreationTimestamp @Column(name = "release_date")
+    var releaseDate: LocalDate,
+    @UpdateTimestamp @Column(name = "updated_on")
+    var updatedOn: LocalDate,
     @Column(name = "token_location")
-    var tokenLocation: String? = null
-
-    @JvmField
-    @ManyToOne
-    @JoinColumn(name = "collection_id")
-    var collection: Collection? = null
-
-    @JsonIgnore // added this and had to fix tx getters and setters for me
+    var tokenLocation: String,
+    @ManyToOne @JoinColumn(name = "collection_id")
+    var collection: Collection,
     @OneToMany(mappedBy = "token")
-    var transfers: List<TokenTx>? = null
-
-    @JvmField
-    @ManyToOne
-    @JoinColumn(name = "creator_id")
-    var creator: User? = null
-
-    @JvmField
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
-    var owner: User? = null
-
-    // update constructors and getters and setters once mappings are added
-    constructor() : super()
-    constructor(
-        id: Int,
-        name: String?,
-        description: String?,
-        price: Int,
-        status: Status?,
-        rarity: Rarity?,
-        releaseDate: LocalDate?,
-        updatedOn: LocalDate?,
-        tokenLocation: String?,
-        collection: Collection?,
-        transfers: List<TokenTx>?,
-        creator: User?,
-        owner: User?
-    ) {
-        this.id = id
-        this.name = name
-        this.description = description
-        this.price = price
-        this.status = status
-        this.rarity = rarity
-        this.releaseDate = releaseDate
-        this.updatedOn = updatedOn
-        this.tokenLocation = tokenLocation
-        this.collection = collection
-        this.transfers = transfers
-        this.creator = creator
-        this.owner = owner
-    }
-
+    var transfers: List<TokenTx>,
+    @ManyToOne @JoinColumn(name = "creator_id")
+    var creator: User,
+    @ManyToOne @JoinColumn(name = "owner_id")
+    var owner: User,
+) : Searchable {
     override fun getType(): String {
         return "Token"
     }
@@ -105,49 +45,12 @@ class Token : Searchable {
     }
 
     override fun getDescription(): String {
-        return description!!
+        return description
     }
 
-    fun setDescription(description: String?) {
-        this.description = description
-    }
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val token = o as Token
-        return id == token.id && price == token.price && name == token.name && description == token.description && status === token.status && rarity === token.rarity && releaseDate == token.releaseDate && updatedOn == token.updatedOn && tokenLocation == token.tokenLocation && collection == token.collection && transfers == token.transfers && creator == token.creator && owner == token.owner
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(
-            id,
-            name,
-            description,
-            price,
-            status,
-            rarity,
-            releaseDate,
-            updatedOn,
-            tokenLocation,
-            collection,
-            transfers,
-            creator,
-            owner
-        )
-    }
-
+    @Override
     override fun toString(): String {
-        return "Token{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", status=" + status +
-                ", rarity=" + rarity +
-                ", releaseDate=" + releaseDate +
-                ", updatedOn=" + updatedOn +
-                ", tokenLocation='" + tokenLocation + '\'' +
-                '}'
+        return this::class.simpleName + "(id = $id , name = $name , description = $description , price = $price , status = $status , rarity = $rarity , owner = $owner )"
     }
+
 }
