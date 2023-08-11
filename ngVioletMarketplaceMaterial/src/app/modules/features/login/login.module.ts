34a31cdec83fd9075f84999ca38506a40aa15e3d
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-10 22:36:55 -0700

    reorganizing, relocated login

--- ngVioletMarketplaceMaterial/src/app/modules/features/login/login.module.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/features/login/login.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { SocialMediaContainerComponent } from './components/social-media-container/social-media-container.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import { MaterialModule } from '../material/material.module';
-import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-import { UserModule } from '../user/user.module';
+import { MaterialModule } from '../../material/material.module';
+import { SharedModule } from '../../shared/shared.module';
+import { UserModule } from '../../user/user.module';
 
 
 
@@ -18,12 +15,9 @@ import { UserModule } from '../user/user.module';
     LoginFormComponent
   ],
   imports: [
-    CommonModule,
     MaterialModule,
     SharedModule,
     UserModule,
-    ReactiveFormsModule,
-    AngularSvgIconModule,
   ],
   exports: [
     LoginComponent
