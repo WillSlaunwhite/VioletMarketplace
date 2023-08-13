8fc72b68a9d9c1476661fc9bd705380b73938529
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-12 22:18:21 -0700

    fixed buttons, mostly

--- ngVioletMarketplaceMaterial/src/app/modules/features/auth/auth.module.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/features/auth/auth.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LayoutModule } from '@angular/cdk/layout';
-import { AuthRoutingModule } from './auth-routing.module';
-import { MaterialModule } from '../../ui/material/material.module';
-import { StoreModule } from '@ngrx/store';
-import { reducer } from '../user/state/user.reducer';
 import { EffectsModule } from '@ngrx/effects';
-import { AuthEffects } from '../user/state/user.effect';
+import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '../../ui/shared/shared.module';
+import { AuthEffects } from '../user/state/user.effect';
+import { reducer } from '../user/state/user.reducer';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
