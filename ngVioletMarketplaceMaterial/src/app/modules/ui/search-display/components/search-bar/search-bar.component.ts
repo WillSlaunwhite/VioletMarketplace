cd3e0960438166842ea3aa5089a5f1a45a4a51fa
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-13 17:59:56 -0700

    stylings for search bar and search container

--- ngVioletMarketplaceMaterial/src/app/modules/ui/search-display/components/search-bar/search-bar.component.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/ui/search-display/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-
-  constructor() { }
+  searchForm: FormGroup;
+  searchTerm: FormControl;
+  isSearchFieldFocused: boolean = false;
+  
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    private router: Router,
+  ) {
+    this.searchTerm = new FormControl('');
+    this.searchForm = new FormGroup({ search: this.searchTerm });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    this.router.navigate(['/results'], { queryParams: { query: this.searchTerm.value } });
+    this.searchTerm.setValue('');
+  }
+
+  toggleSearchFocus() {
+    this.isSearchFieldFocused = !this.isSearchFieldFocused;
+    if (this.isSearchFieldFocused) {
+      this.renderer.addClass(this.el.nativeElement.querySelector('.search'), 'focused');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement.querySelector('.search'), 'focused');
+    }
+  }
 }
--- ngVioletMarketplaceMaterial/src/app/modules/ui/search-display/search-display.module.ts
+++ ngVioletMarketplaceMaterial/src/app/modules/ui/search-display/search-display.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { UserModule } from '../../features/user/user.module';
+import { SharedModule } from '../shared/shared.module';
+import { TokenDisplayModule } from '../tokens/token-display.module';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { DropdownContainerComponent } from './dropdown/dropdown-container/dropdown-container.component';
+import { DropdownTileComponent } from './dropdown/dropdown-tile/dropdown-tile.component';
 import { ResultsComponent } from './results/results.component';
 import { TokenResultsComponent } from './results/token-results/token-results.component';
 import { UserResultsComponent } from './results/user-results/user-results.component';
-import { DropdownContainerComponent } from './dropdown/dropdown-container/dropdown-container.component';
-import { DropdownTileComponent } from './dropdown/dropdown-tile/dropdown-tile.component';
-import { UserModule } from '../../features/user/user.module';
-import { TokenDisplayModule } from '../tokens/token-display.module';
-import { SharedModule } from '../shared/shared.module';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 
