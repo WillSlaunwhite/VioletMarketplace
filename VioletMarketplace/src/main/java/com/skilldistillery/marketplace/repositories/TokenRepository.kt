e8094f5efbb032d50a6189cfa088ef5479923788
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-18 12:52:42 -0700

    getByPopular correctly getting top 10

--- VioletMarketplace/src/main/java/com/skilldistillery/marketplace/repositories/TokenRepository.kt
+++ VioletMarketplace/src/main/java/com/skilldistillery/marketplace/repositories/TokenRepository.kt
@@ -10,9 +10,11 @@ import java.util.*
 interface TokenRepository : JpaRepository<Token, Int>, CustomTokenRepository {
     @Query("SELECT COUNT(t) > 0 FROM Token t WHERE t.owner.username = :username AND t.id = :tokenId")
     fun userOwnsToken(@Param("username") username: String, @Param("tokenId") tokenId: Int): Boolean
+
     @Query("SELECT t FROM Token t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%',:query,'%')) OR LOWER(t.description) LIKE LOWER(CONCAT('%',:query,'%'))")
     fun findByNameOrDescriptionIgnoreCase(@Param("query") query: String): LinkedHashSet<Token>
-    @Query("SELECT t FROM Token t LEFT JOIN t.views v GROUP BY t.id ORDER BY COUNT(v.id) DESC")
+
+    @Query( value = "select t.*, COUNT(v.id) as view_count from token t left join token_view v on t.id = v.token_id group by  t.id, t.name, t.description, t.rarity, t.status, t.release_date, t.updated_on, t.price, t.collection_id, t.owner_id, t.creator_id, t.token_location  order by COUNT(v.id) desc limit 10", nativeQuery = true)
     fun findTop10ByStatusOrderByViewsDesc(status: Status): List<Token>
 
 
